2d974042de02157552ffcb76275ff1eed580912e
Ilya Osipov <osiios.dev@gmail.com>
2022-10-09 15:32:57 +0300

    1. Background image added 2. Labels added 3. Search button added and configured 4. Assets configured 5. AlertController added and configured

---
+++ WeatherApp/Extensions/ViewController+AlertController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController+AlertController.swift
+//  WeatherApp
+//
+//  Created by Илья on 09.10.2022.
+//
+
+import UIKit
+
+extension ViewController {
+    func presentAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        alertController.addTextField { tf in
+            let cities = ["Moscow", "Baku", "Stambul", "New York", "San Francisco"]
+            tf.placeholder = cities.randomElement()
+        }
+        
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = alertController.textFields?.first
+            
+            guard let cityName = textField?.text else { return }
+            
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(search)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
